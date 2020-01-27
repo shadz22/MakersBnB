9825b4ae8e123f3e81055fafac0e6d64d446da89
benauld <ben@portobello-consultancy.co.uk>
2020-01-27 16:14:29 +0000

    changes before process workshop - create / display listings

--- app.rb
+++ app.rb
@@ -5,7 +5,21 @@ require './database_connection_setup'
 
 class BnbApp < Sinatra::Base
 
-  enable :sessions 
+  enable :sessions
+  
+  get '/listings/new' do
+    erb(:new)
+  end
+
+  post '/listings' do
+    Listing.create(name: params[:create_name], description: params[:description], price params[:set_price])
+    redirect '/listings'
+  end
+
+  get '/listings' do
+    erb(:view)
+  end
+
 
   run! if app_file == $0
 end
--- spec/features/features/create_listing_spec.rb
+++ spec/features/features/create_listing_spec.rb
@@ -2,10 +2,10 @@
 
 feature 'Creating a listing' do
   scenario 'on create listing page I want to be able to create a new listing and add to database' do
-    visit('/listing/new')
+    visit('/listings/new')
     fill_in('create_name', with: 'Bob Jones')
     fill_in('description', with: 'Lovely clean room')
-    fill_in('price', with: 50)
+    fill_in('set_price', with: 50)
     click_button('Add Listing')
     expect(page).to have_content 'Bob Jones'
   end
