22c42f1edb6abc6e8a2c8c989c77f09606da81ac
benauld <ben@portobello-consultancy.co.uk>
2020-01-28 15:43:49 +0000

    created table

---
+++ db/migrations/04_create_booking_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bookings(id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), listing_id INTEGER REFERENCES listings(id), start_date DATE NOT NULL, end_date DATE NOT NULL, confirmed BOOLEAN NOT NULL DEFAULT 'FALSE');
--- spec/units/listing_spec.rb
+++ spec/units/listing_spec.rb
@@ -6,9 +6,8 @@ describe Listing do
   let(:subject) { Listing }
   
   before(:each) do
-    DatabaseConnection.setup('bnb_app_test')
+    # DatabaseConnection.setup('bnb_app_test')
   end
-
   let(:user) { User.create(name: 'user1', email: 'email', password: 'password') }
 
   describe '.all' do
