25a89b7f72e03a5cdd509bdbc9ed48f4212c6ce5
David Papamichael <davidpapamichael@gmail.com>
2020-01-31 11:18:30 +0000

    blocked out dates working, booked dates added to database and retrieved

--- lib/booking.rb
+++ lib/booking.rb
@@ -20,12 +20,11 @@ class Booking
   end
 
   def self.listings(listing_id:)
-    p "am I getting to self.listings"
-    p listing_id = listing_id.to_i
+    listing_id = listing_id.to_i
     query = "SELECT * FROM bookings WHERE listing_id = '#{listing_id}'"
     result = DatabaseConnection.query(query)
     array = result.map { |booking| DateHandler.convert(booking['start_date'], booking['end_date']) }
-    p array.flatten
+    array.flatten
   end
 
   def self.create(user_id:, listing_id:, start_date:, end_date:)
